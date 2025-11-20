c3b0a8b1cab97688b4a09e65014da99ef999fe08
hbpeng0115 <hbpeng0115@163.com>
2025-11-20 15:02:00 +0800

    rust integration test

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -10,10 +10,20 @@ pub fn greeting(name: &str) -> String {
     format!("Hello {name}!")
 }
 
+fn internal_adder(left: usize, right: usize) -> usize {
+    left + right
+}
+
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn internal() {
+        let result = internal_adder(2, 2);
+        assert_eq!(result, 4);
+    }
 
     #[test]
     fn it_works() {
@@ -96,10 +106,34 @@ mod tests {
         assert_eq!(value, 10);
     }
 
+    // #[test]
+    // fn this_test_will_fail() {
+    //     let value = prints_and_returns_10(8);
+    //     assert_eq!(value, 5);
+    // }
+
     #[test]
-    fn this_test_will_fail() {
-        let value = prints_and_returns_10(8);
-        assert_eq!(value, 5);
+    fn add_two_and_two() {
+        let result = add_two(2);
+        assert_eq!(result, 4);
+    }
+
+    #[test]
+    fn add_three_and_two() {
+        let result = add_two(3);
+        assert_eq!(result, 5);
+    }
+
+    #[test]
+    fn one_hundred() {
+        let result = add_two(100);
+        assert_eq!(result, 102);
+    }
+
+    #[test]
+    #[ignore]
+    fn expensive_test() {
+        // code that takes an hour to run
     }
 }
 
