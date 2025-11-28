897ce30a6bcd48d054ad58b871e7b0de957e20dd
hbpeng0115 <hbpeng0115@163.com>
2025-11-28 14:45:09 +0800

    rust learning finished

--- match-test/src/main.rs
+++ match-test/src/main.rs
@@ -33,4 +33,28 @@ fn main() {
     for (index, value) in v.iter().enumerate() {
         println!("{value} is at index {index}");
     }
+
+    let numbers = (2, 4, 8, 16, 32);
+
+    match numbers {
+        (first, .., last) => {
+            println!("Some numbers: {first}, {last}");
+        },
+    }
+
+    enum Message {
+        Hello { id: i32 },
+    }
+
+    let msg = Message::Hello { id: 5 };
+
+    match msg {
+        Message::Hello {
+            id: id_variable @ 3..=7,
+        } => println!("Found an id in range: {id_variable}"),
+        Message::Hello { id: 10..=12 } => {
+            println!("Found an id in another range")
+        }
+        Message::Hello { id } => println!("Found some other id: {id}"),
+    }
 }
